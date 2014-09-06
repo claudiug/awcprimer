e56e559e455ade59bf4ad96206da0dc9a3db03f7
claudiu <claudiu.garba@gmail.com>
2014-09-06 12:10:55 +0200

    first commit

---
+++ awcprimer/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  awcprimer
+//
+//  Created by claudiu on 9/6/14.
+//  Copyright (c) 2014 de.claudiu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(void) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
