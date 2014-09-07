16970c019ca96de5039b0cfdcf0629dc9e32808e
claudiu <claudiu.garba@gmail.com>
2014-09-07 19:51:57 +0200

    34

--- awcprimer/getsputs.c
+++ awcprimer/getsputs.c
@@ -1,19 +1,13 @@
-//
-//  getsputs.c
-//  awcprimer
-//
-//  Created by claudiu on 9/6/14.
-//  Copyright (c) 2014 de.claudiu. All rights reserved.
-//
 
 #include <stdio.h>
-#define  STRLEN 20
-int main(void)
+#define STRLEN 20
+int mainz3(void)
 {
     //warning: this program uses gets(), which is unsafe.
     char words[STRLEN];
     printf("Enter a name:\n");
-    gets(words);
+    fgets(words, STRLEN, stdin);
     printf("You enter: %s\n", words);
+    fputs("read me\n", stdout);
     return 0;
 }
